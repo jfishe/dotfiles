77402f32e57568e2f10bc67c50a2f2d7613c7475
John D. Fisher <jdfenw@gmail.com>
2019-01-26 09:59:57 -0800

    fix(bin): undo recursive symlink
    Moved sylinks into local/bin rather than content.
    
    Restore:
        local/bin/colortool.sh
        local/bin/gvim.sh
        local/bin/msysgit2unix-socket.py

--- local/bin/gvim.sh
+++ local/bin/gvim.sh
@@ -1,1 +1,44 @@
-/home/fishe/.dotfiles/local/bin/gvim.sh
+gvim() {
+	local args rawargs path base usegvim=1 noopt=
+	declare -a args
+	rawargs=("$@")
+	while (($#)); do
+		case "$noopt$1" in
+		-[cSdirsTuUwW]|--cmd|--remote-expr|--remote-send|--servername|--version)
+			args[${#args[@]}]="$1"
+			rawargs[${#args[@]}]="$1"
+			shift
+			if (($#)); then
+				args[${#args[@]}]="$1"
+				shift
+			fi
+			;;
+		--)
+			noopt=x
+			args[${#args[@]}]="$1"
+			shift
+			;;
+		-*)
+			args[${#args[@]}]="$1"
+			shift
+			;;
+		*)
+			path=$(realpath -- "$1")
+			if [[ "$path" =~ ^/mnt/[a-z]/ ]]; then
+				base=${path:7}
+				args[${#args[@]}]="${path:5:1}:\\${base/\//\\}"
+			else
+				# fall back to the linux native version
+				usegvim=0
+				break
+			fi
+			shift
+			;;
+		esac
+	done
+	if (($usegvim)); then
+		"/mnt/c/Program Files/Vim/vim81/gvim.exe" "${args[@]}" &
+	else
+		vim "${rawargs[@]}"
+	fi
+}
