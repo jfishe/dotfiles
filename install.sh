c0c19dd86edfab9ffebcc41d7322c684e5b2fcb6
John D. Fisher <jdfenw@gmail.com>
2020-05-03 07:03:09 -0700

    build(install): color xtrace

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# Color xtrace
+local oldPS4=$PS4
+export PS4=$'+\e[33m $BASH_SOURCE:${BASH_LINENO[0]} \e[0m+  '
+
 set -o xtrace
 sudo apt-get update         # Update package database
 
 hash git || sudo apt-get install git
-hash rcup || sudo apt-get install rcm # rc (dotfiles) management
+hash git-lfs || sudo apt-get install git-lfs
+
+# rc (dotfiles) management
+hash rcup || sudo apt-get install rcm
+
+# Used by Vim
 hash ctags || sudo apt install exuberant-ctags # Used by Gutentags in Vim
 hash rg || sudo apt-get install ripgrep
 hash gvim || sudo apt-get install vim-gtk3 # GUI Vim with python3
@@ -12,7 +21,7 @@ hash gvim || sudo apt-get install vim-gtk3 # GUI Vim with python3
 hash tex || sudo apt-get install texlive-full
 
 # Used by shfmt in ALE fixer for bash
-hash go || sudo apt-get install golang 
+hash go || sudo apt-get install golang
 hash shfmt || GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
 
 
@@ -27,8 +36,11 @@ popd
 # echo ''
 # echo 'env RCRC=~/.dotfiles/rcrc rcup # to copy/link dotfiles as specified in rcrc'
 env RCRC=~/.dotfiles/rcrc rcup # to copy/link dotfiles as specified in rcrc
+env RCRC=~/.dotfiles/rcrc rcup # to link dotfiles symlinked to dotfiles
 
 # Update font cache
 fc-cache -vf ~/.local/share/fonts
 
 set +o xtrace
+$PS4="$PS4"
+unlet oldPS4
