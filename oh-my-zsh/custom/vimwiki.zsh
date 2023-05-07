37dbabae84899eeedb597d5cbf318780155039c8
John D. Fisher <jdfenw@gmail.com>
2023-05-07 12:55:44 -0700

    feat(vimwiki): add cli and completion

--- oh-my-zsh/custom/vimwiki.zsh
+++ oh-my-zsh/custom/vimwiki.zsh
@@ -1,11 +1,35 @@
-function vimwiki () {
-    if [[ $# == 0 ]]
-    then
-        gvim +'VimwikiMakeDiaryNote'
-    elif [[ $1 == 'git' ]]
-    then
-        git -C ~/vimwiki/ ${@:2}
-    else
-        echo 'Usage: vimwiki [git] [args ...]'
+#compdef vimwiki
+
+_vimwiki_completion() {
+    local -a completions
+    local -a completions_with_descriptions
+    local -a response
+    (( ! $+commands[vimwiki] )) && return 1
+
+    response=("${(@f)$(env COMP_WORDS="${words[*]}" COMP_CWORD=$((CURRENT-1)) _VIMWIKI_COMPLETE=zsh_complete vimwiki)}")
+
+    for type key descr in ${response}; do
+        if [[ "$type" == "plain" ]]; then
+            if [[ "$descr" == "_" ]]; then
+                completions+=("$key")
+            else
+                completions_with_descriptions+=("$key":"$descr")
+            fi
+        elif [[ "$type" == "dir" ]]; then
+            _path_files -/
+        elif [[ "$type" == "file" ]]; then
+            _path_files -f
+        fi
+    done
+
+    if [ -n "$completions_with_descriptions" ]; then
+        _describe -V unsorted completions_with_descriptions -U
+    fi
+
+    if [ -n "$completions" ]; then
+        compadd -U -V unsorted -a completions
     fi
 }
+
+compdef _vimwiki_completion vimwiki;
+
